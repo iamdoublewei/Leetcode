6054253ef86cf40c2746ff09bfa77c74bf7d1646
Wei Wei <iamdoublewei@gmail.com>
2018-09-22 23:58:46 -0500

    Easy MySQL Practice
    181, 183, 197, 596, 620, 674

---
+++ Python3/674. Longest Continuous Increasing Subsequence.py
@@ -0,0 +1,31 @@
+'''
+Given an unsorted array of integers, find the length of longest continuous increasing subsequence (subarray).
+
+Example 1:
+Input: [1,3,5,4,7]
+Output: 3
+Explanation: The longest continuous increasing subsequence is [1,3,5], its length is 3. 
+Even though [1,3,5,7] is also an increasing subsequence, it's not a continuous one where 5 and 7 are separated by 4. 
+Example 2:
+Input: [2,2,2,2,2]
+Output: 1
+Explanation: The longest continuous increasing subsequence is [2], its length is 1. 
+Note: Length of the array will not exceed 10,000.
+'''
+
+class Solution:
+    def findLengthOfLCIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0: return 0
+        m = 1
+        length = 1
+        for i in range(len(nums)):
+            if i - 1 >= 0 and nums[i] > nums[i - 1]:
+                length += 1
+                m = max(length, m)
+            else:
+                length = 1
+        return m
