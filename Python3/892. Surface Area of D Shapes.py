6a0336969109351ec7cc3a307333504c659703b3
Wei Wei <iamdoublewei@gmail.com>
2018-10-18 22:19:46 -0500

    Easy Practice
    783, 883, 892, 893, 897

---
+++ Python3/892. Surface Area of D Shapes.py
@@ -0,0 +1,53 @@
+'''
+On a N * N grid, we place some 1 * 1 * 1 cubes.
+
+Each value v = grid[i][j] represents a tower of v cubes placed on top of grid cell (i, j).
+
+Return the total surface area of the resulting shapes.
+
+ 
+
+Example 1:
+
+Input: [[2]]
+Output: 10
+Example 2:
+
+Input: [[1,2],[3,4]]
+Output: 34
+Example 3:
+
+Input: [[1,0],[0,2]]
+Output: 16
+Example 4:
+
+Input: [[1,1,1],[1,0,1],[1,1,1]]
+Output: 32
+Example 5:
+
+Input: [[2,2,2],[2,1,2],[2,2,2]]
+Output: 46
+ 
+
+Note:
+
+1 <= N <= 50
+0 <= grid[i][j] <= 50
+'''
+
+class Solution:
+    def surfaceArea(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]:
+                    res += grid[i][j] * 6 - (grid[i][j] - 1) * 2
+                if i > 0:
+                    res -= min(grid[i - 1][j], grid[i][j]) * 2
+                if j > 0:
+                    res -= min(grid[i][j - 1], grid[i][j]) * 2
+        return res
