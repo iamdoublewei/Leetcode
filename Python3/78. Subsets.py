86e1a994a8fcc6bb34799e1113f9b718e645cbee
Wei Wei <iamdoublewei@gmail.com>
2018-10-21 23:52:34 -0500

    Medium Practice
    77, 78, 80, 90

---
+++ Python3/78. Subsets.py
@@ -0,0 +1,31 @@
+'''
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+'''
+
+class Solution:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        for i in range(len(nums) + 1):
+            res += itertools.combinations(nums, i)
+        return res
---
+++ Python3/90. Subsets II.py
@@ -0,0 +1,30 @@
+'''
+Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: [1,2,2]
+Output:
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+'''
+
+class Solution:
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        for i in range(len(nums) + 1):
+            res += list(set(itertools.combinations(nums, i)))
+        return res
