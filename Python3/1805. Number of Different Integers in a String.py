5fcabcea77535a363cb402adcb1f1caec7574bed
Wei Wei <iamdoublewei@gmail.com>
2022-08-30 00:47:43 -0500

    8/30/2022

---
+++ Python3/1805. Number of Different Integers in a String.py
@@ -0,0 +1,50 @@
+'''
+You are given a string word that consists of digits and lowercase English letters.
+
+You will replace every non-digit character with a space. For example, "a123bc34d8ef34" will become " 123  34 8  34". Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
+
+Return the number of different integers after performing the replacement operations on word.
+
+Two integers are considered different if their decimal representations without any leading zeros are different.
+
+ 
+
+Example 1:
+
+Input: word = "a123bc34d8ef34"
+Output: 3
+Explanation: The three different integers are "123", "34", and "8". Notice that "34" is only counted once.
+Example 2:
+
+Input: word = "leet1234code234"
+Output: 2
+Example 3:
+
+Input: word = "a1b01c001"
+Output: 1
+Explanation: The three integers "1", "01", and "001" all represent the same integer because
+the leading zeros are ignored when comparing their decimal values.
+ 
+
+Constraints:
+
+1 <= word.length <= 1000
+word consists of digits and lowercase English letters.
+'''
+
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        num = ''
+        s = set()
+        
+        for letter in word:
+            if letter >= 'a' and letter <= 'z' or letter >= 'A' and letter <= 'Z':
+                num += ' '
+            else:
+                num += letter
+        num = num.split(' ')
+        for n in num:
+            if n != '':
+                s.add(int(n))
+        
+        return len(s)
