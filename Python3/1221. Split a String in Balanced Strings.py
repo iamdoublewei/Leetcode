e28e45e8dea6d6181eeead0b2022b635bb8d3e64
Wei Wei <iamdoublewei@gmail.com>
2022-09-15 07:12:50 -0500

    9/15/2022

---
+++ Python3/1221. Split a String in Balanced Strings.py
@@ -0,0 +1,51 @@
+'''
+Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s, split it into some number of substrings such that:
+
+Each substring is balanced.
+Return the maximum number of balanced strings you can obtain.
+
+ 
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", each substring contains same number of 'L' and 'R'.
+Note that s cannot be split into "RL", "RR", "RL", "LR", "LL", because the 2nd and 5th substrings are not balanced.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+ 
+
+Constraints:
+
+2 <= s.length <= 1000
+s[i] is either 'L' or 'R'.
+s is a balanced string.
+'''
+
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        l_cnt = 0
+        r_cnt = 0
+        cnt = 0
+        
+        for letter in s:
+            if letter == 'L':
+                l_cnt += 1
+            else:
+                r_cnt += 1
+            if (l_cnt and r_cnt) and (l_cnt == r_cnt):
+                cnt += 1
+                l_cnt = r_cnt = 0
+                
+        return cnt
